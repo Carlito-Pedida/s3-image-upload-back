15653bb6cecfb38277542deecc24567aa9271005
Carlito Pedida Jr <carlped417@gmail.com>
2024-01-07 21:42:41 -0600

    aws bucket setup

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { json } from "express";
 import cors from "cors";
 import multer, { memoryStorage } from "multer";
+import { uploadToS3 } from "./s3.mjs";
 
 const app = express();
 
@@ -20,11 +21,13 @@ app.use(json());
 app.post("/images", upload.single("image"), (req, res) => {
   const { file } = req;
   const userId = req.headers["x-user-id"];
-
   console.log(file);
   if (!file || !userId) return res.status(400).json({ message: "Bad request" });
 
-  return res.send("success");
+  const { error, key } = uploadToS3({ file, userId });
+  if (error) return res.status(500).json({ message: error.message });
+
+  return res.status(201).json({ key });
 });
 
 app.listen(PORT, () => {
