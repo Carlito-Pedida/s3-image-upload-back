daa2d60316f1af1348ec7e49666e09c00b264979
Carlito Pedida Jr <carlped417@gmail.com>
2024-01-07 23:22:58 -0600

    final mod

--- s3.mjs
+++ s3.mjs
@@ -1,4 +1,10 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  ListObjectsV2Command,
+  PutObjectCommand,
+  S3Client
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { v4 as uuid } from "uuid";
 
 const s3 = new S3Client();
@@ -16,6 +22,39 @@ export const uploadToS3 = async ({ file, userId }) => {
   try {
     await s3.send(command);
     return { key };
+  } catch (error) {
+    console.log(error);
+    return { error };
+  }
+};
+
+const getImageKeysByUser = async (userId) => {
+  const command = new ListObjectsV2Command({
+    Bucket: BUCKET,
+    Prefix: userId
+  });
+
+  const { Contents = [] } = await s3.send(command);
+
+  return Contents.sort(
+    (a, b) => new Date(b.LastModified) - new Date(a.LastModified)
+  ).map((image) => image.Key);
+};
+
+export const getUserPresignedUrls = async (userId) => {
+  try {
+    const imageKeys = await getImageKeysByUser(userId);
+
+    const presignedUrls = await Promise.all(
+      imageKeys.map((key) => {
+        const command = new GetObjectCommand({
+          Bucket: BUCKET,
+          Key: key
+        });
+        return getSignedUrl(s3, command, { expiresIn: 900 });
+      })
+    );
+    return { presignedUrls };
   } catch (error) {
     console.log(error);
     return { error };
